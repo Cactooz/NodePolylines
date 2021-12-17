d991928028f78501e9dbb0876a24eceacad9069c
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-17 13:26:20 +0100

    Check that there are nodes

--- src/NPolyline.java
+++ src/NPolyline.java
@@ -110,18 +110,20 @@ public class NPolyline implements Polyline{
 	public void addPoint(Point newPoint, String pointName) {
 		Node n = firstNode;
 		
-		while(!Objects.equals(firstNode.point.getName(), pointName) || !Objects.equals(n.nextNode.point.getName(), pointName)) {
-			n = n.nextNode;
+		while (n != null) {
+			while(!Objects.equals(firstNode.point.getName(), pointName) || !Objects.equals(n.nextNode.point.getName(), pointName)) {
+				n = n.nextNode;
+			}
+			
+			//Save the next node
+			Node tmpNextNode = n.nextNode;
+			
+			//Change the next node to the new node
+			n.nextNode = new Node(new Point(newPoint));
+			
+			//Set the newPoints next node to the old nextNode
+			n.nextNode.nextNode = tmpNextNode;
 		}
-		
-		//Save the next node
-		Node tmpNextNode = n.nextNode;
-		
-		//Change the next node to the new node
-		n.nextNode = new Node(new Point(newPoint));
-		
-		//Set the newPoints next node to the old nextNode
-		n.nextNode.nextNode = tmpNextNode;
 	}
 	
 	//Remove a point defined by its frame from the array
