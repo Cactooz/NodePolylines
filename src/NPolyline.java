f89c70c177483b0efce0e9b16cb0e9d44bc0a9ec
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-17 10:25:36 +0100

    Constructor for node taking array with points

--- src/NPolyline.java
+++ src/NPolyline.java
@@ -16,4 +16,16 @@ public class NPolyline implements Polyline{
 	public NPolyline() {
 		point = null;
 	}
+	
+	public NPolyline(Point[] points) {
+		if(points.length > 0) {
+			Node node = new Node(new Point(points[0]));
+			point = node;
+			int pos = 1;
+			while(pos < points.length) {
+				node.nextNode = new Node(new Point(points[pos++]));
+				node = node.nextNode;
+			}
+		}
+	}
 }
