2d265703c2d143fd12db0915d0695bc6a8d13294
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-17 10:15:48 +0100

    Added node class

--- src/NPolyline.java
+++ src/NPolyline.java
@@ -1,3 +1,11 @@
 public class NPolyline implements Polyline{
-
+	public static class Node {
+		public Point point;
+		public Node nextNode;
+		
+		public Node(Point point) {
+			this.point = point;
+			nextNode = null;
+		}
+	}
 }
